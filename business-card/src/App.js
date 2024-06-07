9a1c8b0bcf70f052c4b4884e07c7353726a882ef
Cr1ppl3 <53353378+UnkownC1ppl3@users.noreply.github.com>
2024-06-08 09:14:58 +1000

    0

---
+++ business-card/src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+
+import React from 'react';
+import BusinessCard from './BusinessCard';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <BusinessCard />
+    </div>
+  );
+};
+
+export default App;
